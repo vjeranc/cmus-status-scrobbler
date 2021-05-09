e905000b156c6666d79fa7044a893b16b5a1c25a
Vjeran Crnjak <vjeran@crnjak.xyz>
2021-05-09 23:40:31 +0200

    use scrobbler.py in cmus_status_scrobbler.py

--- cmus_status_scrobbler.py
+++ cmus_status_scrobbler.py
@@ -1,19 +1,20 @@
-import sys
-import time
-import urllib
 import argparse
 import configparser
-import os
 from collections import namedtuple
-import logging
 import datetime
+import hashlib
+import json
+import logging
+import os
+import sys
+import time
+import urllib.parse as up
+import urllib.request as ur
 
 CONFIG_PATH = '~/.config/cmus/cmus_status_scrobbler.ini'
 
 parser = argparse.ArgumentParser(description="Scrobbling.")
-parser.add_argument('--ini',
-                    type=argparse.FileType('r'),
-                    default=os.path.expanduser(CONFIG_PATH))
+parser.add_argument('--ini', type=str, default=os.path.expanduser(CONFIG_PATH))
 parser.add_argument('--now-playing', type=bool, default=True, required=False)
 parser.add_argument('--api-url', type=str, required=False)
 parser.add_argument('--auth-url', type=str, required=False)
@@ -44,15 +45,29 @@ Status = namedtuple('Status', [
 ])
 
 
+def get_api_sig(params, secret=None):
+    m = hashlib.md5()
+    for k in sorted(params):
+        m.update(k.encode('utf-8'))
+        m.update(params[k].encode('utf-8'))
+    m.update(secret.encode('utf-8'))
+    return m.hexdigest()
+
+
 class Scrobbler:
+    def __init__(self, api_key, shared_secret):
+        self.api_key = api_key
+        self.shared_secret = shared_secret
+
     def auth():
         pass
 
     def scrobble():
         pass
 
-    def send_now_playing():
-        pass
+    def send_now_playing(cur_status):
+        if cur_status.status != CmusStatus.playing:
+            return
 
 
 class ScrobbleCache:
@@ -114,20 +129,73 @@ def calculate_scrobbles(status_updates, perc_thresh=0.5, secs_thresh=4 * 60):
         # files are equal and status paused
 
 
+class ScrobblerMethod:
+    GET_TOKEN = 'auth.gettoken'
+    GET_SESSION = 'auth.getsession'
+    NOW_PLAYING = 'track.updateNowPlaying'
+
+
+def send_req(api_url, api_key, shared_secret=None, method=None, **params):
+    params = dict(**params)
+    params['api_key'] = api_key
+    params['method'] = method
+    if shared_secret:
+        params['api_sig'] = get_api_sig(params, secret=shared_secret)
+    params['format'] = 'json'
+    api_req = ur.Request(api_url, headers={"User-Agent": "Mozilla/5.0"})
+
+    with ur.urlopen(api_req, up.urlencode(params).encode('utf-8')) as f:
+        return json.loads(f.read().decode('utf-8'))
+
+
+def authenticate(auth_url, api_url, api_key, shared_secret):
+    # fetching token that is used to ask for access
+    # headers= makes it work on libre.fm
+    token = send_req(api_url, api_key,
+                     method=ScrobblerMethod.GET_TOKEN)['token']
+    print(f'{auth_url}?api_key={api_key}&token={token}')
+    input('Press <Enter> after visiting the link and allowing access...')
+    # fetching session with infinite lifetime that is used to scrobble
+    session = send_req(api_url,
+                       api_key,
+                       shared_secret=shared_secret,
+                       method=ScrobblerMethod.GET_SESSION,
+                       token=token)['session']
+    return dict(session_key=session['key'], username=session['name'])
+
+
 if __name__ == "__main__":
     logging.basicConfig(
         filename='/home/vjeran/.config/cmus/cmus_scrobbler.log',
         encoding='utf-8',
         level=logging.DEBUG,
     )
-    # args = parser.parse_args()
-    # print(args)
-    # conf_parser = configparser.ConfigParser()
-    # print(conf_parser.sections())
-    # conf_parser.read_file(args.ini)
-    # print(conf_parser.sections())
-    # args.ini.close()
-    # print(conf_parser.items('global'))
+    args = parser.parse_args()
+    print(args)
+    conf_path = args.ini
+    if not os.path.exists(conf_path):
+        raise FileNotFoundError(f'{conf_path} does not exist.')
+
+    conf = configparser.ConfigParser()
+    with open(conf_path, 'r') as f:
+        conf.read_file(f)
+    api_key, shared_secret = None, None  # using global if local not defined
+    for section in conf.sections():
+        if section == 'global':
+            api_key = conf[section]['api_key']
+            shared_secret = conf[section]['shared_secret']
+            continue
+        if 'session_key' in conf[section]:
+            print(f'Session key already active for {section}. Skipping...')
+            continue
+        conf[section].update(
+            authenticate(
+                conf[section]['auth_url'], conf[section]['api_url'],
+                conf[section].get('api_key', None) or api_key,
+                conf[section].get('shared_secret', None) or shared_secret))
+        with open(conf_path, 'w') as f:
+            conf.write(f)
+
     try:
         logging.info(repr(parse_cmus_status_line(sys.argv[1:])))
     except Exception as e:
